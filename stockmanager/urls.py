a53c9bfe22de032faa10b62f522ec68442992e0f
jtvandijk <j.t.vandijk@ucl.ac.uk>
2026-06-13 17:07:02 +0100

    update urls

--- stockmanager/urls.py
+++ stockmanager/urls.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include, reverse_lazy
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 from inventory.forms import StyledLoginForm
 
+
 urlpatterns = [
+    path('', lambda request: redirect('/inventory/')),
     path('admin/', admin.site.urls),
     path('inventory/', include('inventory.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=StyledLoginForm),name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='login.html',
+            authentication_form=StyledLoginForm
+        ),
+        name='login'
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(
+            next_page=reverse_lazy('login')
+        ),
+        name='logout'
+    ),
 ]
