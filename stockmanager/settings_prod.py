448252086b008d5458bcc497ea35f759e560bbee
jtvandijk <j.t.vandijk@ucl.ac.uk>
2026-06-13 16:34:11 +0100

    static url server

--- stockmanager/settings_prod.py
+++ stockmanager/settings_prod.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = ["178.79.153.239", "127.0.0.1", "localhost"]
 
 # STATIC FILES (required for production)
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "/static/"
+STATIC_URL = "/inventory/static/"
 
 # Extra security (safe defaults)
 SECURE_BROWSER_XSS_FILTER = True
