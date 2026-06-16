64d55dcbbf662ba8b572fd6214ad1842e2c64611
jtvandijk <j.t.vandijk@ucl.ac.uk>
2026-06-16 12:29:08 +0100

    set autofield default to silence migration warnings

--- stockmanager/settings.py
+++ stockmanager/settings.py
@@ -116,6 +116,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
